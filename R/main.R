63d05aac53caf61656181fa956908132c3eff255
Pedro Augusto Borges dos Santos <pabsantos21@gmail.com>
2024-05-02 13:13:19 -0300

    Finaliza a análise global da autocorrelação

--- R/main.R
+++ R/main.R
@@ -1,5 +1,6 @@
 library(arrow)
 library(sf)
+library(aopdata)
 
 ndsbr_sample <- arrow::open_dataset("data/ndsbr_sample.parquet")
 
@@ -15,8 +16,36 @@ filtered_speed_sample <-
   ]
 
 ## Transformando em dados espaciais com o `sf`
-speed_points <- st_as_sf(
+speed_points <- sf::st_as_sf(
   filtered_speed_sample,
   coords = c("long", "lat"),
   crs = "4674"
 )
+
+## Considerando apenas velocidades acima de 10 km/h
+speed_points_filtered <- speed_points[speed_points$spd_kmh > 10, ]
+
+## Importando a base h3 de Curitiba com auxílio do aopdata
+grid_cwb <- aopdata::read_grid(city = "cur")
+
+## Unindo os dados do nds-br com o grid
+speed_poits_grid <- sf::st_join(
+  sf::st_set_crs(speed_points_filtered, 4326),
+  grid_cwb
+)
+
+## Calculando a velocidade média por grid e agregando os novos dados
+## Nota: depois calcular desvio padrao, mediana, 85 quantil,
+mean_speed <- tapply(speed_poits_grid$spd_kmh, speed_poits_grid$id_hex, mean)
+
+mean_speed_df <- data.frame(
+  id_hex = names(mean_speed),
+  mean_speed = mean_speed,
+  row.names = NULL
+)
+
+grid_cwb_mean_spd <- merge(grid_cwb, mean_speed_df, by = "id_hex", all.x = TRUE)
+
+plot(grid_cwb_mean_spd["mean_speed"])
+
+hist(speed_points_filtered$spd_kmh, breaks = 30)
